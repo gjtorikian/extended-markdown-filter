d47bf0ed646f32ca3d162cbde91e730a7eb20b9b
Garen Torikian <gjtorikian@gmail.com>
2014-11-25 11:03:11 -0800

    Provide EMF overrides for Jekyll Pages, too

--- lib/jekyll-override.rb
+++ lib/jekyll-override.rb
@@ -6,11 +6,24 @@ module Jekyll
     alias_method :old_run, :run
 
     def run
-      html_pipeline_context = site.site_payload["site"]["html_pipeline"] && site.site_payload["site"]["html_pipeline"]["context"]
-      if site.site_payload["site"]["markdown"] == "HTMLPipeline" && html_pipeline_context && site.site_payload["site"]["html_pipeline"]["context"][:emf_use_blocks]
+      if ExtendedMarkdownFilter.should_jekyll_replace?(site)
         document.content = ExtendedMarkdownFilter.convert_curly_to_bracket(document.content)
       end
       old_run
+    end
+
+  end
+end
+
+module Jekyll
+  class Page
+    alias_method :old_render, :render
+
+    def render(layouts, site_payload)
+      if ExtendedMarkdownFilter.should_jekyll_replace?(site)
+        self.content = ExtendedMarkdownFilter.convert_curly_to_bracket(content)
+      end
+      old_render(layouts, site_payload)
     end
   end
 end
