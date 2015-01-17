246b759330694e49e6015491baabab510bca5917
Sheri Bernard <bernars@github.com>
2015-01-16 18:16:28 -0800

    Add new note admonition

--- lib/filters/post/admonition.rb
+++ lib/filters/post/admonition.rb
@@ -2,9 +2,10 @@ module Filters
   module PostFilter
     def format_admonitions!(html)
       html.gsub!(/<p>#{Filters.front_wrap}\s*#tip\s*#{Filters.end_wrap}<\/p>/,     '<div class="alert tip">')
+      html.gsub!(/<p>#{Filters.front_wrap}\s*#note\s*#{Filters.end_wrap}<\/p>/,    '<div class="alert note">')
       html.gsub!(/<p>#{Filters.front_wrap}\s*#warning\s*#{Filters.end_wrap}<\/p>/, '<div class="alert warning">')
       html.gsub!(/<p>#{Filters.front_wrap}\s*#error\s*#{Filters.end_wrap}<\/p>/,   '<div class="alert error">')
-      html.gsub!(/<p>#{Filters.front_wrap}\s*\/(tip|warning|error)\s*#{Filters.end_wrap}<\/p>/, '</div>')
+      html.gsub!(/<p>#{Filters.front_wrap}\s*\/(tip|note|warning|error)\s*#{Filters.end_wrap}<\/p>/, '</div>')
     end
   end
 end
