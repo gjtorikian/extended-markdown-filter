ac490339f3aa21f181a11ab5c9ea38587818fe20
Sheri Bernard <bernars@github.com>
2015-01-16 18:17:05 -0800

    Rename 'error' to 'danger', take it or leave it

--- lib/filters/post/admonition.rb
+++ lib/filters/post/admonition.rb
@@ -4,8 +4,8 @@ module Filters
       html.gsub!(/<p>#{Filters.front_wrap}\s*#tip\s*#{Filters.end_wrap}<\/p>/,     '<div class="alert tip">')
       html.gsub!(/<p>#{Filters.front_wrap}\s*#note\s*#{Filters.end_wrap}<\/p>/,    '<div class="alert note">')
       html.gsub!(/<p>#{Filters.front_wrap}\s*#warning\s*#{Filters.end_wrap}<\/p>/, '<div class="alert warning">')
-      html.gsub!(/<p>#{Filters.front_wrap}\s*#error\s*#{Filters.end_wrap}<\/p>/,   '<div class="alert error">')
-      html.gsub!(/<p>#{Filters.front_wrap}\s*\/(tip|note|warning|error)\s*#{Filters.end_wrap}<\/p>/, '</div>')
+      html.gsub!(/<p>#{Filters.front_wrap}\s*#danger\s*#{Filters.end_wrap}<\/p>/,   '<div class="alert danger">')
+      html.gsub!(/<p>#{Filters.front_wrap}\s*\/(tip|note|warning|danger)\s*#{Filters.end_wrap}<\/p>/, '</div>')
     end
   end
 end
