d4a534f2e4dd3d62fe4c604ccaf8cc817e098952
Garen Torikian <gjtorikian@gmail.com>
2015-11-20 15:41:46 -0800

    Get rid of a mysterious span

--- lib/filters/pre/command-line.rb
+++ lib/filters/pre/command-line.rb
@@ -4,9 +4,9 @@ module Filters
       text.gsub /\n?``` command-line(.+?)```/m do |block|
         block.gsub! /^``` command-line/, '<pre class="command-line">'
         block.gsub! /^```$/, "</pre>\n"
-        block.gsub!(/^\$ (.+)$/) { %Q|<span class="command">#{$1.rstrip}</span>| }
-        block.gsub!(/^(\# .+)$/) { %Q|<span class="comment">#{$1.rstrip}</span>| }
-        block.gsub!(/^> (.+)$/) { %Q|<span class="output"><span># </span>#{$1.rstrip}</span>| }
+        block.gsub!(/^\$ (.+)$/) { %(<span class="command">#{$1.rstrip}</span>) }
+        block.gsub!(/^(\# .+)$/) { %(<span class="comment">#{$1.rstrip}</span>) }
+        block.gsub!(/^> (.+)$/) { %(<span class="output">#{$1.rstrip}</span>) }
 
         block
       end
